338ba7ebfbca46086b64067916d76466d7717399
artem4k4823 <artemdv2000@gmail.com>
2026-03-20 09:37:19 +0700

    fix ports

--- backend/main.py
+++ backend/main.py
@@ -42,4 +42,4 @@ authentication_backend = AdminAuth(secret_key=settings.SECRET_KEY)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
